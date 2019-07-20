2763539f8719085e7be86d390648370d8be76c10
unknown <nwafordabere@gmail.com>
2019-07-20 23:42:27 +0100

    changed yelp_camp port number

--- app.js
+++ app.js
@@ -47,6 +47,6 @@ app.use(indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(3000, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log("The YelpCamp Server is listening!");
 })
