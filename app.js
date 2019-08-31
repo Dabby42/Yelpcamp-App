539c74d121157902ac9ff07fbdd88136e3fc9e2a
unknown <nwafordabere@gmail.com>
2019-08-31 16:21:06 +0100

    Edited footer

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
+    port        = 3000 || "process.env.PORT",
     passport    = require("passport"),
     flash       = require("connect-flash"),
     passportLocalMongoose = require("passport-local-mongoose"),
@@ -49,6 +50,6 @@ app.use(indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(port, process.env.IP, function(){
   console.log("The YelpCamp Server is listening!");
 })
